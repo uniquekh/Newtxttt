2dc54e068c01862f59dbd0adf70c7aeb7894f24c
darindaop <125014338+darindaop@users.noreply.github.com>
2023-06-16 22:55:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config(object):
     # get a token from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6043046734:AAGWoYZEmuXkzP6oJic72vHKPrel17mngC0")
     API_ID = int(os.environ.get("API_ID", "12782359"))
-    API_HASH = os.environ.get("API_HASH", "17bc32a1b9c376919578129806554d2e)
+    API_HASH = os.environ.get("API_HASH", "17bc32a1b9c376919578129806554d2e")
     AUTH_USERS = "2026665680"
 
